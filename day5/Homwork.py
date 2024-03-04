76522be93f70278a303b49d89e142381d54d792d
akoako999 <akotabatadze009@gmail.com>
2024-03-04 16:27:19 +0400

    fift commit

---
+++ day5/Homwork.py
@@ -0,0 +1,22 @@
+nam1 = input("enter you name: ")
+nam2 = int(input( "enter you age "))
+nam3 = int(input( "enter mother age "))
+
+print (type (nam1))
+print (type (nam2))
+print (type (nam3))
+
+print (nam2 / nam3)
+print(nam2 // nam3)
+print (nam2 % nam3)
+
+
+
+
+
+
+
+
+
+
+
