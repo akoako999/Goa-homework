62baa25ec0a0979fa7462ea6862ed3391b00e4c7
akoako999 <akotabatadze009@gmail.com>
2024-03-27 14:38:32 +0400

    commit

---
+++ day7/ახალი საქაღალდე/Homwork.py
@@ -0,0 +1,32 @@
+# celsius_temp = float(input("შეიყვანე ტემპერატურა ცელსიუსში"))
+
+
+# fahreneit_temp = (celsius_temp * 1.8) + 32
+
+# print("The temperature in Fahreit is:", fahreneit_temp) #davaleba1
+
+
+# user_age = int(input ("შეიტანე შენი ასაკი:")) #დავალება2
+# print(user_age)
+
+# print(user_age < 20 and user_age > 12)    
+
+
+user_value1 = int(input("შემოიტანე მართკუთხედი ractangle 1 გვერდი"))  # davaleba3
+user_value2 = int(input("შემოიტანე მართკუთხედი ractangle 2 გვერდი"))
+                   
+print(user_value1 / user_value2) 
+
+# user_number = int(input("შემოიტანე რიცხვი"))
+# print(user_number)
+
+
+# print(user_number > 100 or user_number * 18 )   #davaleba4
+
+
+
+
+
+
+
+
