4824a889502087549e81e5ffd6810c5aca39effd
akoako999 <akotabatadze009@gmail.com>
2024-04-08 18:39:08 +0400

    commit

---
+++ day9/classwork1/homwork/homwork.py
@@ -0,0 +1,7 @@
+salary = input( "hom mutch do you have salary?: ")
+if salary >= 10000:
+    print( "do you learn in GOA ")
+elif salary >= 1000 < 10000:
+    print ("your mid")
+else:
+    print ("join GOA")
