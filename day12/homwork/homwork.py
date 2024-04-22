ebb33a8cd1f59bcd87d58ebf1235a4252b7444b4
akoako999 <akotabatadze009@gmail.com>
2024-04-22 16:29:31 +0400

    commit

---
+++ day12/homwork/homwork.py
@@ -0,0 +1,23 @@
+# for i in range(11):
+#     if i % 2 != 0:
+#         print(i)
+
+
+
+
+# for i in range(50):
+#     if i % 5  == 0:
+#         print(i)
+
+
+
+# nam1=int(input("გამარჯობა გთხოვთ შემოიტანოთ რაიმე რიცხვი"))
+# for i in range(0,nam1):
+#     print(i)
+
+
+total = 0
+for i in range(51):
+    total += i
+print  (total)    
+
