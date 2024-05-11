e343f0f77805337e26580401c72981758d96d54c
akoako999 <akotabatadze009@gmail.com>
2024-05-11 13:40:38 +0400

    commit

---
+++ day14/homwork/homwork.py
@@ -0,0 +1,22 @@
+# for i in range (51): 
+#     if i  % 5==0: 
+#         print (i) 
+
+# for i in range (50): 
+#     if i % 5==1:
+#         print (i)
+
+  
+nam = 0
+while nam < 51: 
+    if nam % 5 == 0:
+        print (nam)
+    nam += 1
+    
+        
+        
+
+        
+
+
+
